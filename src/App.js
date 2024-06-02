e3c7437735c84af87c5e4c810541b0df0501e2ba
Yesid Rincon <dairo.rincon@uptc.edu.co>
2024-06-02 18:45:41 -0500

    add verify login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Layout from './layout/Layout'
+import { AuthContext } from './context/AuthContext'
+import Login from './components/page/Login'
 
 const App = () => {
+  const {user} = useContext(AuthContext)
+  if(!user){
+    return (<Login/>)
+  }
   return (
     <Layout />
   )
